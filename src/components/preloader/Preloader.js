57265b8d7a98c49fe60decdff096f7e4d7a24864
Rudransh Khandelwal <rudranshkhandelwal123@gmail.com>
2022-03-09 16:19:29 +0530

    fix gradient container background

--- src/components/preloader/Preloader.js
+++ src/components/preloader/Preloader.js
@@ -3,7 +3,7 @@ import '../preloader/Preloader.css'
 import SyncLoader from "react-spinners/SyncLoader";
 function Preloader({loading}) {
     return (
-        <div className="main-container">
+        <div className="pre-container">
          <div className='l'>
          <h1>
             <span style={{color: "#d82148"}}>Color</span>
